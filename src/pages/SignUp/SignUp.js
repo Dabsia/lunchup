5622f0941cea99ea64e68515c93cf75b85d2624e
Dabsia <dabojohnson@gmail.com>
2022-04-30 14:51:48 -0700

    Created the sign up functinoality using firebase

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React , {useRef, useState} from 'react'
 import { Link } from 'react-router-dom';
 import logo from '../../assets/favicon.png'
 import ImageBanner from '../../components/ImageBanner/ImageBanner';
@@ -6,6 +6,59 @@ import './SignUp.css';
 
 
 const SignUp = () => {
+
+  const API_KEY = 'AIzaSyB2h3x72iA_O6-0Rx_mqXOdqhj1MvZWOKY';
+
+  // add Loading when creating a request
+  const [isLoading, setIsLoading] = useState(false)
+
+  // Get user inputs on signup
+  const emailInputRef = useRef()
+  const usernameInputRef = useRef()
+  const phoneNumberInputRef = useRef()
+  const passwordInputRef = useRef()
+  
+// Create Account Function
+  const submitHandler = (e) => {
+    e.preventDefault()
+
+    const enteredUsername = usernameInputRef.current.value
+    const enteredEmail = emailInputRef.current.value
+    const enteredPhone = phoneNumberInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value
+
+
+
+    setIsLoading(true)
+    fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true
+      }),
+      headers: {
+        'Content-Type' : 'application/json'
+      }
+    }).then(res => {
+      setIsLoading(false)
+      if (res.ok){
+        console.log('Account Created')
+      }
+      else{
+        return res.json().then(data => {
+          let errorMessage = 'Authentication Failed'
+          if (data && data.error && data.error.message) {
+            errorMessage = data.error.message
+          }
+          alert(errorMessage)
+        });
+      }
+    })
+
+  }
+
+
   return (
     <div className='sign'>
       <ImageBanner />
@@ -15,25 +68,25 @@ const SignUp = () => {
           <img src = {logo} alt = 'logo' />
         </div>
         <h1 className='auth__text'>Create an Account</h1>
-        <form>
+        <form onSubmit={submitHandler}>
           <div className='form_detail'>
             <label htmlFor='username'> Username</label>
-            <input type = 'text' id = 'username' className='form_input' placeholder='Username' />
+            <input type = 'text' ref = {usernameInputRef} id = 'username' className='form_input' placeholder='Username' />
           </div>
           <div className='form_detail'>
             <label htmlFor='number'> Phone Number</label>
-            <input type = 'number' id = 'phone' className='form_input' placeholder='Phone Number' />
+            <input type = 'number' ref = {phoneNumberInputRef} id = 'phone' className='form_input' placeholder='Phone Number' />
           </div>
           <div className='form_detail'>
             <label htmlFor='email'> Email</label>
-            <input type = 'email' id = 'email' className='form_input' placeholder='Email' />
+            <input type = 'email' id = 'email' ref = {emailInputRef} className='form_input' placeholder='Email' />
           </div>
           <div className='form_detail'>
             <label htmlFor='password'> Password</label>
-            <input type = 'password' id = 'password' className='form_input' placeholder='password' />
+            <input type = 'password' ref = {passwordInputRef} id = 'password' className='form_input' placeholder='password' />
           </div>
           <div className='btn'>
-            <button type='submit' className='form_button'>Create Account</button>
+            <button type='submit' className='form_button'>{!isLoading ? 'Create Account': 'Loading...'}</button>
           </div>
         </form>
         <p className='haveAcctAlready'>Already have an account? <Link className='logIn' to  = '/signin'>Log In</Link></p>
