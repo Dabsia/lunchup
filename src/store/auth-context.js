727da86f657205f39cec1e698e0c108a365fae25
Dabsia <dabojohnson@gmail.com>
2022-04-30 23:49:16 -0700

    Persisting the user state using localStorage

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -9,17 +9,21 @@ const AuthContext = React.createContext({
 
 
 export const AuthContextProvider = (props) => {
+    const initialToken = localStorage.getItem('token')
 
-    const [token, setToken] = useState(null)
+    const [token, setToken] = useState(initialToken)
 
     const userIsLoggedIn = !!token;
 
     const loginHandler = (token) => {
-        setToken(token)
+        setToken(token);
+        localStorage.setItem('token', token)
+
     }
 
     const logoutHandler = () => {
-        setToken(null)
+        setToken(null);
+        localStorage.removeItem('token')
     }
 
     const contextValue = {
