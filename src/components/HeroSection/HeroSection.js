90ac6c712976ffaa7375fb671da5b46b02c7cfbf
Dabsia <dabojohnson98@gmail.com>
2022-05-20 14:56:58 -0700

    Finished the heroSection

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,17 +1,35 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import './HeroSection.css'
 import chips_image from '../../assets/Layer 2.png'
 import small_burger from '../../assets/smallburger.png'
+import AuthContext from '../../store/auth-context'
+import { useNavigate } from 'react-router-dom'
 
 
 const HeroSection = () => {
+
+    const Navigate = useNavigate()
+
+    const authCtx = useContext(AuthContext)
+    console.log(authCtx.isLoggedIn)
+
+    const HomeRedirect = () => {
+        if (authCtx.isLoggedIn === true) {
+            Navigate('/shop')
+        }
+        else {
+            Navigate('/signin')
+        }
+        
+    }
+
   return (
       <div className='heroSection'>
           <h1 className='heroSectionHeader'>Order Food, Fast, Easy & Convenient</h1>
           <div className='btnContainer'>
-            <button className='orderNowBtn'>Order Now</button>
+            <button onClick={HomeRedirect} className='orderNowBtn'>Order Now</button>
           </div>
-          <img src={chips_image} className='chips_image' alt='chips image' />
+          <img src={chips_image} className='chips_image' alt='chips' />
           <img src = {small_burger} className = 'small_burger'  alt = 'small_burger'  />
       </div>
   )
