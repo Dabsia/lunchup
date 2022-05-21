efc16cf84fc34a967b3684594467ce75aba19622
Dabsia <dabojohnson98@gmail.com>
2022-05-21 15:33:07 -0700

    Made the vendors component so it could accomodate all the vendors

--- src/components/HowItWorks/HowItWorks.js
+++ src/components/HowItWorks/HowItWorks.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import './HowItWorks.css'
 
 const HowItWorks = () => {
   return (
-    <div>HowItWorks</div>
+      <div className='howItWorks'>
+          <div className='vendors'>
+            <h3 className='vendors__desc'>Order from the best food stations around you</h3>
+          </div>
+          <div className='partners'>
+
+          </div>
+          <div className='howItWorksContainer'>
+              <h3 className='vendors__desc'>How it works</h3>
+              <div className='divine'>
+              </div>
+          </div>
+      </div>
   )
 }
 
--- src/pages/Welcome/Welcome.js
+++ src/pages/Welcome/Welcome.js
@@ -1,7 +1,6 @@
 import React from 'react' 
 // import AppDesc from '../../components/AppDesc/AppDesc'
 import Navigation from '../../components/Navigation/Navigation'
-import small_Image from '../../assets/smallImage.png'
 import HowItWorks from '../../components/HowItWorks/HowItWorks'
 import HeroSection from '../../components/HeroSection/HeroSection'
 
@@ -10,9 +9,6 @@ const Welcome = () => {
     
       <div>
       <Navigation />
-      <div className = 'small_ImageCont'>
-          <img className = 'small_Image' src = {small_Image} alt = 'smallImage' />
-      </div>
       <HeroSection />
       <HowItWorks />
   {/*<AppDesc />*/}
