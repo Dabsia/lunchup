4f0056793cfe5676a47dfe03e5cf42820c31c52c
Dabsia <Dabojohnson98@gmail.com>
2022-05-24 15:45:43 -0700

    Made the app fully responsive

--- src/components/HowItWorks/HowItWorks.js
+++ src/components/HowItWorks/HowItWorks.js
@@ -3,31 +3,27 @@ import './HowItWorks.css'
 import rect from '../../assets/Rectangle 133.png'
 import rect2 from '../../assets/Rectangle 134.png'
 import rectPhone from '../../assets/Group 217.png'
+
 const HowItWorks = () => {
   return (
       <div className='howItWorks'>
-          <div className='vendors__box'>
-            <h3 className='vendors__desc'>Order from the best food stations around you</h3>
-          </div>
-          <div className='partners'>
-
-          </div>
+          
           <div className='howItWorksContainer'>
-              <h3 className='vendors__desc'>How it works</h3>
-        <div className='vendors__desc--image'>
-          <div>
-            <img src={rect}  className = 'howItWorksImages' alt='rect' /> 
-            <p className='orderFoodText'>Choose a food vendor</p>
-          </div>
-          <div>
-            <img src={rectPhone} className = 'howItWorksImages'  alt='rectPhone' /> 
-            <p className='orderFoodText'>Place an Order</p>
-          </div>
-          <div>
-            <img src={rect2} className = 'howItWorksImages'  alt='rect2' /> 
-            <p className='orderFoodText'>Food is delivered to You</p>
-          </div>
-        </div>
+            <h3 className='vendors__desc'>How it works</h3>
+              <div className='vendors__desc--image'>
+                <div className='howItWorksImagesHolder'>
+                  <img src={rect}  className = 'howItWorksImages' alt='rect' /> 
+                  <p className='orderFoodText'>Choose a food vendor</p>
+                </div>
+                <div className='howItWorksImagesHolder'>
+                  <img src={rectPhone} className = 'howItWorksImages'  alt='rectPhone' /> 
+                  <p className='orderFoodText'>Place an Order</p>
+                </div>
+                <div className='howItWorksImagesHolder'>
+                  <img src={rect2} className = 'howItWorksImages'  alt='rect2' /> 
+                  <p className='orderFoodText'>Food is delivered to You</p>
+                </div>
+              </div>
           </div>
       </div>
   )
