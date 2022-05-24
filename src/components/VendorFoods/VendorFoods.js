a9775e8367e3536d2577eaf261e42313210edef3
Dabsia <Dabojohnson98@gmail.com>
2022-05-24 16:40:37 -0700

    Downloaded the images required

--- src/components/VendorFoods/VendorFoods.js
+++ src/components/VendorFoods/VendorFoods.js
@@ -3,13 +3,15 @@ import Navigation from '../Navigation/Navigation'
 import { useParams } from 'react-router-dom'
 
 
+
 const VendorFoods = () => {
 
   const { vendorId } = useParams();
   return (
     <div>
       <Navigation />
-      <h2>{ vendorId }</h2>
+      <h2>{vendorId}</h2>
+  
     </div>
   )
 }
