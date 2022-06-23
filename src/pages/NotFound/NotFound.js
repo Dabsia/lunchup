0a29bf6ccc6edd7dfb490ebd1db73f04f287d18c
Dabsia <Dabojohnson98@gmail.com>
2022-06-23 11:55:11 -0700

    Styled the empty page

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import emptyImage from '../../assets/no-food-available 2.png'
+import Button from '../../components/Button/Button'
+import './NotFound.css'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className='notFoundContainer'>
+      <div className = 'notFoundItems'>
+        <img className='notFoundImage' src={emptyImage} alt='No Food Found' />
+        <p className='notFoundText'>The page you are looking for doesn't exist</p>
+        <Button text = 'Go back Home'/>
+      </div>
+    </div>
   )
 }
 
